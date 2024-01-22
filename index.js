ca0ff81089ac98271d170e4d7041cafc36d729d5
Dima Ryabov <dima.poltergeist.2015@yandex.ru>
2024-01-22 13:44:02 +0300

    поправил адаптив

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const burgerBtn = document.querySelector(".header-buttons__burger");
 const closeBtn = document.querySelector(".popup-close__icon");
 const popup = document.querySelector(".header__popup");
+const btnFixed = document.querySelector(".services__button");
 
 burgerBtn.addEventListener("click", () => {
   popup.style.display = "flex";
